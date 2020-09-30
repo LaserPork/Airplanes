fbd3cf89cfcc63640193aee42edd7696f3a0032f
vaverkaj <vaverkaja@gmail.com>
2020-09-30 20:47:25 +0200

    First physics

--- Assets/Scripts/AirplaneController.cs
+++ Assets/Scripts/AirplaneController.cs
@@ -4,17 +4,31 @@ using UnityEngine;
 
 public class AirplaneController : MonoBehaviour
 {
-    public float force = 0;
+    public float torque = 0.5f;
+    public float force = 10;
     public Rigidbody2D airplaneBody;
     // Start is called before the first frame update
     void Start()
     {
         airplaneBody = GetComponent<Rigidbody2D>();
+        Collider2D airplaneCollider = GetComponent<Collider2D>();
+        airplaneBody.centerOfMass += new Vector2(airplaneCollider.bounds.size.x / 10,0);
     }
 
     // Update is called once per frame
     void Update()
     {
-        airplaneBody.AddForce(new Vector2(force,0));
+        if(Input.GetAxis("Horizontal") > 0)
+        {
+            airplaneBody.AddTorque(-torque);
+        }else if(Input.GetAxis("Horizontal") < 0)
+        {
+            airplaneBody.AddTorque(torque);
+        }
+
+        if(Input.GetKey("space"))
+        {
+            airplaneBody.AddRelativeForce(new Vector2(force,0));
+        }
     }
 }
--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -4,26 +4,64 @@ using UnityEngine;
 
 public class CameraFollow : MonoBehaviour
 {
+
+    private Camera currentCamera;
     public GameObject[] following;
     public GameObject ground;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        currentCamera = GetComponent<Camera>();
     }
 
     // Update is called once per frame
     void Update()
     {
         Vector3 followingCenter = new Vector3(0,0,0);
+        Vector2 minValue = new Vector2(float.MaxValue, float.MaxValue);
+        Vector2 maxValue = new Vector2(float.MinValue, float.MinValue);
         for(int i = 0; i < following.Length; i++)
         {
             followingCenter += following[i].transform.position;
+            if(following[i].transform.position.x < minValue.x)
+            {
+                minValue.x = following[i].transform.position.x;
+            }
+            if(following[i].transform.position.y < minValue.y)
+            {
+                minValue.y = following[i].transform.position.y;
+            }
+            if(following[i].transform.position.x > maxValue.x)
+            {
+                maxValue.x = following[i].transform.position.x;
+            }
+            if(following[i].transform.position.y > maxValue.y)
+            {
+                maxValue.y = following[i].transform.position.y;
+            }
         }
         followingCenter /= following.Length;
         followingCenter.z = -10;
 
         gameObject.transform.position = followingCenter;
+
+
+        float vertExtent = currentCamera.orthographicSize;    
+        float horzExtent = vertExtent * Screen.width / Screen.height;
+        
+        float requiredWidth = maxValue.x - minValue.x;
+        float requiredHeight = maxValue.y - minValue.y;
+
+    
+        if(requiredWidth - horzExtent < requiredHeight - vertExtent)
+        {
+            currentCamera.orthographicSize = requiredHeight * 1.2f;
+        }
+        else
+        {
+            currentCamera.orthographicSize = requiredWidth * 1.2f * Screen.height / Screen.width;
+        }
+        
     }
 }
