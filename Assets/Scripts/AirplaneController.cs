a45c3be7736e362291dcb54545d326539dd8501c
vaverkaj <vaverkaja@gmail.com>
2020-09-30 15:16:17 +0200

    First follow camera script

--- Assets/Scripts/AirplaneController.cs
+++ Assets/Scripts/AirplaneController.cs
@@ -4,16 +4,17 @@ using UnityEngine;
 
 public class AirplaneController : MonoBehaviour
 {
-    public GameObject Airplane;
+    public float force = 0;
+    public Rigidbody2D airplaneBody;
     // Start is called before the first frame update
     void Start()
     {
-        
+        airplaneBody = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        airplaneBody.AddForce(new Vector2(force,0));
     }
 }
---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public GameObject[] following;
+    public GameObject ground;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 followingCenter = new Vector3(0,0,0);
+        for(int i = 0; i < following.Length; i++)
+        {
+            followingCenter += following[i].transform.position;
+        }
+        followingCenter /= following.Length;
+        followingCenter.z = -10;
+
+        gameObject.transform.position = followingCenter;
+    }
+}
